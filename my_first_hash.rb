fb6695f609dc3a5100ca63463589565d236900aa
Dakota Wicker <dwicker1996@gmail.com>
2020-03-29 02:02:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,15 +18,16 @@ def retrieval
 
 end
 
+def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"]=>2
-  shipping_manisfest["gun powder"]=>4
+                 shipping_manifest["muskets"]=>2
+                 shipping_manisfest["gun powder"]=>4
 
-  puts shipping_manifest
+                 shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
